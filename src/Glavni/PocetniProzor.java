ffe4d53c8986fa489803dec4b94b748128a90da1
unknown <lukamomcilovic221099@gmail.com>
2021-09-08 16:21:27 +0200

    exportovana aplikacija

--- src/Glavni/PocetniProzor.java
+++ src/Glavni/PocetniProzor.java
@@ -58,71 +58,71 @@ public class PocetniProzor {
 		frame.getContentPane().setLayout(null);
 		
 		unosBrPoljaM = new JTextField();
-		unosBrPoljaM.setBounds(163, 189, 46, 20);
+		unosBrPoljaM.setBounds(163, 180, 46, 20);
 		frame.getContentPane().add(unosBrPoljaM);
 		unosBrPoljaM.setColumns(10);
 		
 		unosBrPoljaN = new JTextField();
-		unosBrPoljaN.setBounds(231, 189, 46, 20);
+		unosBrPoljaN.setBounds(231, 180, 46, 20);
 		frame.getContentPane().add(unosBrPoljaN);
 		unosBrPoljaN.setColumns(10);
 		
 		unosBrMina = new JTextField();
-		unosBrMina.setBounds(299, 189, 46, 20);
+		unosBrMina.setBounds(299, 180, 46, 20);
 		frame.getContentPane().add(unosBrMina);
 		unosBrMina.setColumns(10);
 		
 		Panel panel = new Panel();
 		panel.setBackground(new Color(0, 102, 255));
-		panel.setBounds(0, 0, 373, 39);
+		panel.setBounds(0, 0, 383, 39);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
 		lblNewLabel = new JLabel("Миноловац");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblNewLabel.setForeground(new Color(255, 255, 255));
-		lblNewLabel.setBounds(10, 0, 103, 39);
+		lblNewLabel.setBounds(10, 0, 150, 39);
 		panel.add(lblNewLabel);
 		
 		Panel panel_1 = new Panel();
 		panel_1.setBackground(new Color(221, 221, 221));
-		panel_1.setBounds(0, 35, 373, 46);
+		panel_1.setBounds(0, 35, 383, 46);
 		frame.getContentPane().add(panel_1);
 		panel_1.setLayout(null);
 		
 		JLabel lblNewLabel_2 = new JLabel("Висина");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_2.setBounds(157, 23, 57, 23);
+		lblNewLabel_2.setBounds(157, 23, 69, 23);
 		panel_1.add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_2_1 = new JLabel("Ширина");
 		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_2_1.setBounds(224, 23, 57, 23);
+		lblNewLabel_2_1.setBounds(224, 23, 69, 23);
 		panel_1.add(lblNewLabel_2_1);
 		
 		JLabel lblNewLabel_2_1_1 = new JLabel("Мине");
 		lblNewLabel_2_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_2_1_1.setBounds(291, 23, 57, 23);
+		lblNewLabel_2_1_1.setBounds(291, 23, 82, 23);
 		panel_1.add(lblNewLabel_2_1_1);
 		
 		JRadioButton radio1 = new JRadioButton("   Лако ");
 		radio1.setFont(new Font("Tahoma", Font.BOLD, 17));
-		radio1.setBounds(0, 87, 109, 23);
+		radio1.setBounds(0, 87, 157, 23);
 		frame.getContentPane().add(radio1);
 		
 		JRadioButton radio2 = new JRadioButton("   Средње ");
 		radio2.setFont(new Font("Tahoma", Font.BOLD, 17));
-		radio2.setBounds(0, 117, 121, 23);
+		radio2.setBounds(0, 117, 157, 23);
 		frame.getContentPane().add(radio2);
 		
 		JRadioButton radio3 = new JRadioButton("   Тешко");
 		radio3.setFont(new Font("Tahoma", Font.BOLD, 17));
-		radio3.setBounds(0, 152, 121, 23);
+		radio3.setBounds(0, 147, 157, 23);
 		frame.getContentPane().add(radio3);
 		
 		JRadioButton radio4 = new JRadioButton("   Унеси сам");
 		radio4.setFont(new Font("Tahoma", Font.BOLD, 17));
-		radio4.setBounds(0, 186, 139, 23);
+		radio4.setBounds(0, 177, 157, 23);
 		frame.getContentPane().add(radio4);
 		
 		ButtonGroup group = new ButtonGroup();
@@ -164,30 +164,30 @@ public class PocetniProzor {
 		
 		JLabel lblNewLabel_1_3_1 = new JLabel("16");
 		lblNewLabel_1_3_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_1_3_1.setBounds(163, 152, 23, 20);
+		lblNewLabel_1_3_1.setBounds(163, 147, 23, 20);
 		frame.getContentPane().add(lblNewLabel_1_3_1);
 		
 		JLabel lblNewLabel_1_1_1_1 = new JLabel("30");
 		lblNewLabel_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_1_1_1_1.setBounds(231, 152, 23, 20);
+		lblNewLabel_1_1_1_1.setBounds(231, 147, 23, 20);
 		frame.getContentPane().add(lblNewLabel_1_1_1_1);
 		
 		JLabel lblNewLabel_1_2_1_1 = new JLabel("99");
 		lblNewLabel_1_2_1_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_1_2_1_1.setBounds(299, 152, 23, 20);
+		lblNewLabel_1_2_1_1.setBounds(299, 147, 23, 20);
 		frame.getContentPane().add(lblNewLabel_1_2_1_1);
 		
 		Panel panel_1_1 = new Panel();
 		panel_1_1.setLayout(null);
 		panel_1_1.setBackground(new Color(221, 221, 221));
-		panel_1_1.setBounds(0, 215, 373, 46);
+		panel_1_1.setBounds(0, 215, 383, 56);
 		frame.getContentPane().add(panel_1_1);
 		
-		Button button = new Button("Нова игра");
-		button.setFont(new Font("Dialog", Font.BOLD, 17));
+		Button button = new Button("Start");
+		button.setFont(new Font("Dialog", Font.BOLD, 18));
 		button.setBackground(new Color(0, 102, 255));
 		button.setForeground(new Color(255, 255, 255));
-		button.setBounds(10, 7, 147, 32);
+		button.setBounds(10, 8, 112, 32);
 		panel_1_1.add(button);
 		button.addMouseListener(new MouseAdapter() {
 			@Override
